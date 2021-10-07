a5a1e239bccb4cdb2f69370903c4e2467b682824
Jéssika Gomes <64968527+jessikagomes@users.noreply.github.com>
2021-10-07 09:02:55 -0300

    refactoring code

--- src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
+++ src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
@@ -1,29 +1,64 @@
 package br.com.chronosAcademy.appium;
 import io.appium.java_client.MobileBy;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import java.net.MalformedURLException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTest {
 
-    @Test
-    public void validarMultiplicacao() throws MalformedURLException {
-        DriverFactory driverFactory = new DriverFactory();
-        driverFactory.setCapabilities("Android","ChronosAcademy","uiautomator2","com.android.calculator2","com.android.calculator2.Calculator");
+    DriverFactory driverFactory;
 
+    @BeforeEach
+    public void iniciarDriver() throws MalformedURLException {
+        driverFactory = new DriverFactory();
+        driverFactory.setCapabilities("Android","ChronosAcademy","uiautomator2","com.android.calculator2","com.android.calculator2.Calculator");
         driverFactory.setDriver();
-        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
-        driverFactory.getDriver().findElement(MobileBy.id("op_mul")).click();
-        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+    }
+
+    @AfterEach
+    public void finalizarDriver(){
+        driverFactory.getDriver().quit();
+    }
+
+    @Test
+    @DisplayName("Validar Multiplicação 7 x 9 resultado 63")
+    public void validarMultiplicacao() {
+
+        clickId("digit_7");
+        clickId("op_mul");
+        clickId("digit_9");
+        clickId("eq");
+
+        Integer resultado = Integer.parseInt(driverFactory.getDriver().findElement(MobileBy.id("result")).getText());
+
+        assertEquals(63,resultado);
+
+    }
+
+    @Test
+    @DisplayName("Validar Adição 25 + 45 resultado 70")
+    public void validarAdicao() throws MalformedURLException {
+
+        driverFactory.getDriver().findElement(MobileBy.id("digit_2")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_5")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_add")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_4")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_5")).click();
         driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
 
-        Integer resultado = driverFactory.getDriver().findElement(MobileBy.id("result"));
+        Integer resultado = Integer.parseInt(driverFactory.getDriver().findElement(MobileBy.id("result")).getText());
 
-        assertEquals("63",resultado);
+        assertEquals(70,resultado);
 
-        driverFactory.getDriver().quit();
+    }
 
 
+    public void clickId(String id){
+        driverFactory.getDriver().findElement(MobileBy.id(id)).click();
+    }
 
+    public void getResult(String id){
+        Integer resultado = Integer.parseInt(driverFactory.getDriver().findElement(MobileBy.id(id)).getText());
     }
 }
